1606e85b7cd69cab5f363b16ccd98285364bfdf6
Daria <daria359@gmail.com>
2020-03-29 02:55:41 +0100

    Add a way to chain methods.
    Found a weird issue with "return this" in add/remove/toggle/hasClass methods. Add a comment about it in Attributes.js

--- src/Library/Attributes.js
+++ src/Library/Attributes.js
@@ -3,39 +3,51 @@ import { isObject } from "../Utils/Check.js";
 export default {
 
     attr(key, val) {
-        if(key !== undefined && val === undefined) {
+        if (key !== undefined && val === undefined) {
             return this.get(0).getAttribute(key);
         } else {
-            this.each( node => node.setAttribute(key, val) )
+            return this.each(node => node.setAttribute(key, val))
         }
     },
 
     addClass(className) {
-        this.each( node => node.classList.add(className) );
+        // this way everything works properly:
+        this.each(node => node.classList.add(className));
+        return this;
+        // but written this way doesnt:
+        // return this.each(node => node.classList.add(className));
+        // it adds a class but it doesnt returns proper this (node)
+        // so when one wants to chain methods, like that:
+        // node.addClass("red").css("font-size", "20px");
+        // second method won't work
     },
-    
+
     removeClass(className) {
-        return this.each( node => node.classList.remove(className) );
+        this.each(node => node.classList.remove(className));
+        return this
     },
-    
+
     toggleClass(className) {
-        return this.each( node => node.classList.toggle(className) );
+        this.each(node => node.classList.toggle(className));
+        return this
     },
-    
+
     hasClass(className) {
-        return this.each( node => node.classList.has(className) );
+        this.each(node => node.classList.has(className));
+        return this
     },
 
     css(prop, val) {
-        if(prop !== undefined && val !== undefined) {
-            this.each( node => node.style[prop] = val );
-        } else if( isObject(prop) ) {
-            for(let key in prop) {
-                this.each( node => node.style[key] = prop[key] );
+        if (prop !== undefined && val !== undefined) {
+            this.each(node => node.style[prop] = val);
+        } else if (isObject(prop)) {
+            for (let key in prop) {
+                this.each(node => node.style[key] = prop[key]);
             }
         } else {
             return this.get(0).style[prop];
         }
+        return this;
     }
 
 }
--- src/test.js
+++ src/test.js
@@ -4,15 +4,24 @@ export default function() {
     // lis.html("<b>new</b> text")
     // console.log(lis.html());
 
-    lis.on("click", function () {
-        console.log( this );
+    lis.toggleClass("red").css("font-size", "20px");
+
+    lis.on("click", function() {
+        console.log(this);
 
         // console.log( this.text() );
-        /* this won't work, because our methods are naot avaliable on native objects! 
-        ownQuery methods are avaliable on ownQuery objects! */
+        /* 
+        this won't work, because our methods are naot avaliable on native objects! 
+        ownQuery methods are avaliable on ownQuery objects! 
+        */
 
-        console.log( ownQuery(this).text() );
-        /* to work with ownQuery methods we need to wrap native object with our ownQuery object. 
-        To do this, we can use our ownQuery() method which looks up for the element and return an array with tis el*/
+        console.log(ownQuery(this).text());
+        /* 
+        to work with ownQuery methods we need to wrap native object with our ownQuery object. 
+        To do this, we can use our ownQuery() method which looks up for the element and return an array with tis el
+        */
     })
+
+
+
 }
